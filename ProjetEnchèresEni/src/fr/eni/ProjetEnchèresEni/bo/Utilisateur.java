0bdc3a0f7546bc67798948e4353a0f45a2065b8c
mirela <mirela@mirelas-mbp>
2021-01-05 14:33:38 +0100

    Classe Utilisateur BO

--- ProjetEnchèresEni/src/fr/eni/ProjetEnchèresEni/bo/Utilisateur.java
+++ ProjetEnchèresEni/src/fr/eni/ProjetEnchèresEni/bo/Utilisateur.java
@@ -1,5 +1,147 @@
 package fr.eni.ProjetEnchèresEni.bo;
 
 public class Utilisateur {
+	
 
+
+
+	private String noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private int codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
+	private String utilisateur;
+	
+	//constructeur à vide
+	public Utilisateur() {
+	}
+	
+	//constructeur avec tous les attributs
+	public Utilisateur(String noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, int codePostal, String ville, String motDePasse, int credit, String utilisateur) {
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.utilisateur = utilisateur;
+
+}
+
+	public String getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(String noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public int getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public String getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(String utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [noUtilisateur=" + noUtilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom="
+				+ prenom + ", email=" + email + ", telephone=" + telephone + ", rue=" + rue + ", codePostal="
+				+ codePostal + ", ville=" + ville + ", motDePasse=" + motDePasse + ", credit=" + credit
+				+ ", utilisateur=" + utilisateur + "]";
+	}
+	
 }
