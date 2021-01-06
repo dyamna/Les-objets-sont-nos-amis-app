29746aa0cb3059f262f957820be4b0efa1e9ef56
farid <farid@LAPTOP-AJ97LP6R>
2021-01-06 23:01:58 +0100

    LoginServlet.

--- ProjetEnchèresEni/src/fr/eni/ProjetEnchèresEni/servlets/LoginServlet.java
+++ ProjetEnchèresEni/src/fr/eni/ProjetEnchèresEni/servlets/LoginServlet.java
@@ -6,36 +6,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Utilisateur
+ * Servlet implementation class Login
  */
-@WebServlet("/Utilisateur")
-public class UtilisateurServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	
+	
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UtilisateurServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+		String name=request.getParameter("Login");
+		String password=request.getParameter("password");
+		
+		if(name.equals("nawal")&&password.equals("Password"));
+		{
+			HttpSession session = request.getSession();
+			session.setAttribute("Login", name);
+		}
+			
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
+		
+
