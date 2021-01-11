7fc8a9efdf1346cf20198841b0e1dd702bbf236e
farid <farid@LAPTOP-AJ97LP6R>
2021-01-11 15:42:17 +0100

    Package messages.

---
+++ ProjetEnchèresEni/src/fr/eni/ProjetEnchèresEni/messages/LecteurMessages.java
@@ -0,0 +1,58 @@
+package fr.eni.ProjetEnchèresEni.messages;
+
+
+import java.util.ResourceBundle;
+	
+
+	/**
+	 * Cette classe permet de lire le contenu du fichier messages_erreurs.properties
+	 *
+	 *
+	 */
+	public class LecteurMessages {
+		private static ResourceBundle rb;
+		
+		static
+		{
+			try
+			{
+				rb = ResourceBundle.getBundle("fr.eni.projetencheres.messages.messages_erreur");
+			}
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		private LecteurMessages()
+		{
+			
+		}
+		/**
+		 * @param code
+		 * @return
+		 */
+		public static  String getMessageErreur(int code)
+		{
+			String message="";
+			try
+			{
+				if(rb!=null)
+				{
+					message = rb.getString(String.valueOf(code));
+				}
+				else
+				{
+					message="Problème à la lecture du fichier contenant les messages";
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				message="Une erreur inconnue est survenue";
+			}
+			System.out.println("message="+message);
+			return message;
+		}
+	}
+
+
